45711dcc5fc7ef49752422b4ca0e01654e059be9
Richard Ma <richard.ma.19850509@gmail.com>
2018-12-02 15:27:01 +0800

    TODO

---
+++ win4mall/design/backend/templates/views/payments/components/cc_processors/win4mall.php
@@ -0,0 +1,13 @@
+<div class="control-group">
+    <label class="control-label" for="accno">Sfepay Account:</label>
+    <div class="controls">
+        <input type="text" name="payment_data[processor_params][accno]" id="accno" value="{$processor_params.accno}" >
+    </div>
+</div>
+
+<div class="control-group">
+    <label class="control-label" for="md5key">Sfepay MD5Key:</label>
+    <div class="controls">
+        <input type="text" name="payment_data[processor_params][md5key]" id="md5key" value="{$processor_params.md5key}" >
+    </div>
+</div>
